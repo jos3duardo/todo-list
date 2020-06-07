24fec24e418a0f05060135611c33cd5bd6d06d73
jos3duardo <jos3duardolopes@gmail.com>
2020-06-07 15:42:27 -0400

    criando modal para cadastro de tarefas

---
+++ web/src/components/layouts/ModalTaskVue.vue
@@ -0,0 +1,33 @@
+<template>
+    <div :id="identificador" class="modal">
+        <div class="modal-content">
+            <h4>{{titulo}}</h4>
+            <slot name="content"></slot>
+        </div>
+        <div class="modal-footer">
+            <slot name="footer"></slot>
+<!--            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fecha</a>-->
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Modal',
+    props: ['identificador','titulo'],
+    data(){
+        return {
+
+        }
+    },
+    mounted() {
+        let elems = document.querySelectorAll('.modal');
+        let instances = M.Modal.init(elems);
+    }
+}
+</script>
+<style>
+    .modal {
+        width: 50%;
+        height: 35%;
+    }
+</style>
